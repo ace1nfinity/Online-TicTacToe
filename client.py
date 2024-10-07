56ed9204b9ac7e100bfa98643ec6f22c8062e933
ace1nfinity <134473021+ace1nfinity@users.noreply.github.com>
2024-10-06 19:36:00 -0600

    Client and Server Update. Added requirements.txt

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
 import sys
 import socket
 import selectors
+import traceback
+import struct
+
+import TTTclient
 
 sel = selectors.DefaultSelector()
 
 def create_request(action, value):
+    packedData = struct.pack('>6si', action.encode(), int(value))
     return dict(
-        type="text/json",
-        encoding="utf-8",
-        content=dict(action=action, value=value),
+        type="binary/custom-client-binary-type",
+        encoding="binary",
+        content=packedData
     )
 
 def start_connection(host, port, request):
@@ -18,24 +23,35 @@ def start_connection(host, port, request):
     sock.setblocking(False)
     sock.connect_ex(addr)
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    #message = libclient.Message(sel, sock, addr, request)
+    message = TTTclient.Message(sel, sock, addr, request)
     sel.register(sock, events, data=None)
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 5:
     print("usage:", sys.argv[0], "<host> <port>")
     sys.exit(1)
 
-def start_connection(host, port, request):
-    addr = (host, port)
-    print("starting connection to", addr)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(False)
-    sock.connect_ex(addr)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    #message = libclient.Message(sel, sock, addr, request)
-    sel.register(sock, events, data=None)
-
 host, port = sys.argv[1], int(sys.argv[2])
-action, value = 10, 20
+action, value = "test", 20
 request = create_request(action, value)
 start_connection(host, port, request)
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        for key, mask in events:
+            message = key.data
+            try:
+                message.process_events(mask)
+            except Exception:
+                print(
+                    "main: error: exception for",
+                    f"{message.addr}:\n{traceback.format_exc()}",
+                )
+                message.close()
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import sys
 import socket
 import selectors
+import traceback
+
+import TTTserver
 
 sel = selectors.DefaultSelector()
 
@@ -8,7 +11,7 @@ def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
     print("accepted connection from", addr)
     conn.setblocking(False)
-    message = libserver.Message(sel, conn, addr)
+    message = TTTserver.Message(sel, conn, addr)
     sel.register(conn, selectors.EVENT_READ, data=message)
 
 if len(sys.argv) != 3:
@@ -25,3 +28,24 @@ lsock.listen()
 print("listening on", (host, port))
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                message = key.data
+                try:
+                    message.process_events(mask)
+                except Exception:
+                    print(
+                        "main: error: exception for",
+                        f"{message.addr}:\n{traceback.format_exc()}",
+                    )
+                    message.close()
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
